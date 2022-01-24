d399e18a64d6b6d04d4407f7699c0cbaaf7af24b
ShadowUser17 <linuxforce17@gmail.com>
2022-01-24 12:39:42 +0200

    Changes to be committed: 	new file:   docker/install-docker.sh 	new file:   lima/ubuntu.yaml 	new file:   user/readme.txt

---
+++ docker/install-docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+[[ $UID -ne 0 ]] && {
+    echo -e "You must be root!"; exit 1
+}
+
+GPG_KEY="/usr/share/keyrings/docker-archive-keyring.gpg"
+APT_REPO="/etc/apt/sources.list.d/docker.list"
+
+OS_NAME=`awk -F= '$1~/^ID$/{print $2}' /etc/os-release`
+OS_ARCH=`dpkg --print-architecture`
+OS_RELEASE=`awk -F= '$1~/^VERSION_CODENAME$/{print $2}' /etc/os-release`
+
+
+apt-get update && \
+apt-get install -y \
+ca-certificates gnupg curl lsb-release \
+|| exit $?
+
+
+[[ ! -f "${GPG_KEY}" ]] && \
+curl -fsSL "https://download.docker.com/linux/debian/gpg" | gpg --dearmor -o "${GPG_KEY}"
+
+
+[[ ! -f "${APT_REPO}" ]] && echo "deb [arch=${OS_ARCH} signed-by=${GPG_KEY}] \
+https://download.docker.com/linux/debian ${OS_RELEASE} stable" > "${APT_REPO}"
+
+
+[[ $? -eq 0 ]] && \
+apt-get update && \
+apt-get install -y \
+docker-ce docker-ce-cli containerd.io \
+|| exit $?
+
+apt-get clean
---
+++ user/readme.txt
@@ -0,0 +1,1 @@
+curl -L 'https://raw.githubusercontent.com/ShadowUser17/BasicInstalls/master/user/debian_root_bashrc' -o '/root/.bashrc'
