d4682f08c015fcc2c81ceee8296ebde0755f57ee
ShadowUser17 <linuxforce17@gmail.com>
2022-06-25 15:36:40 +0300

    Changes to be committed: 	new file:   grafana/grafana-datasources/alertmanager.yml 	new file:   grafana/grafana-datasources/prometheus.yml 	new file:   grafana/install-grafana.sh

---
+++ grafana/install-grafana.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+[[ "${UID}" -ne "0" ]] && {
+    echo -e "You must be root!"; exit 1
+}
+
+PKG_VERSION="9.0.1"
+PKG_FILE="grafana-enterprise_${PKG_VERSION}_amd64.deb"
+PKG_URL="https://dl.grafana.com/enterprise/release/${PKG_FILE}"
+
+# Install Grafana...
+apt-get install -y adduser libfontconfig1 && \
+curl -L "${PKG_URL}" -o "${HOME}/${PKG_FILE}" && \
+dpkg -i "${HOME}/${PKG_FILE}"
+
+# Install data sources...
+cp -v grafana-datasources/*.yml /etc/grafana/provisioning/datasources/ && \
+chown root:grafana /etc/grafana/provisioning/datasources/*.yml && \
+chmod 640 /etc/grafana/provisioning/datasources/*.yml
