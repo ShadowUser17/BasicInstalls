6f6e71c025fb18c4bb9b61a8a50393034cacc7ca
ShadowUser17 <linuxforce17@gmail.com>
2022-07-04 19:53:24 +0300

    Changes to be committed: 	modified:   prometheus/install-blackbox-exporter.sh 	modified:   prometheus/install-node-exporter.sh 	new file:   prometheus/install-vector.sh

---
+++ prometheus/install-vector.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[[ "${UID}" -ne "0" ]] && {
+    echo -e "You must be root!"; exit 1
+}
+
+VECTOR_VERSION="0.22.3"
+VECTOR_FILE="vector_${VECTOR_VERSION}-1_amd64.deb"
+VECTOR_URL="https://github.com/vectordotdev/vector/releases/download/v${VECTOR_VERSION}/${VECTOR_FILE}"
+
+
+[[ -f "${VECTOR_FILE}" ]] && rm -f "${VECTOR_FILE}"
+
+curl -O -L "${VECTOR_URL}" && dpkg -i "${VECTOR_FILE}"
