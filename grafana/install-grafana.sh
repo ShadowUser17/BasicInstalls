388bbb7d4ca7054012bdb9495ee5c3ec6349155f
ShadowUser17 <linuxforce17@gmail.com>
2023-04-26 12:13:36 +0300

    Changes to be committed: 	new file:   exporters/vector.service 	modified:   grafana/install-grafana.sh

--- grafana/install-grafana.sh
+++ grafana/install-grafana.sh
@@ -4,7 +4,7 @@
     echo -e "You must be root!"; exit 1
 }
 
-PKG_VERSION="9.2.15"
+PKG_VERSION="9.2.17"
 PKG_FILE="grafana-enterprise_${PKG_VERSION}_amd64.deb"
 PKG_URL="https://dl.grafana.com/enterprise/release/${PKG_FILE}"
 
